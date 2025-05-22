a328807965b1062c20709fc4e788b66fd3bf55cb
Gus Glover <gus.glov3r@gmail.com>
2025-05-22 14:18:46 -0700

    Structure vs stack deliniation

--- infra/lib/cdk-stack.ts
+++ infra/lib/cdk-stack.ts
@@ -1,24 +1,18 @@
 import * as cdk from "aws-cdk-lib";
-import * as sqs from "aws-cdk-lib/aws-sqs";
-import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
+import { Stack } from "aws-cdk-lib";
+import { StaticSite } from "./static-site";
+import { StatsLambda } from "./stats-lambda";
 
-export class CdkStack extends cdk.Stack {
+export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create the S3 bucket
-    const bucket = new s3.Bucket(this, "amzn-s3-demo-bucket", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    new StaticSite(this, "site", {
+      siteSubDomain: "www",
+      domainName: "baseball.com",
     });
 
-    // Create an output for the bucket's Region
-    new cdk.CfnOutput(this, "BucketRegion", {
-      value: bucket.env.region,
-    });
-
-    const queue = new sqs.Queue(this, "CdkQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
-    });
+    new StatsLambda(this, "stats-lambda", {});
   }
 }
